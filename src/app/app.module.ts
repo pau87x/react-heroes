e070057ab65c2409e9ac4a7d240628c1caec804a
Paulina Elizalde <pau87x@gmail.com>
2017-02-24 13:56:35 -0600

    Adding route to dashboard, and redirect index to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
+
 import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
@@ -21,8 +24,17 @@ import { HeroService } from './hero.service';
     HttpModule,
     RouterModule.forRoot([
       {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
       }
     ])
   ],
